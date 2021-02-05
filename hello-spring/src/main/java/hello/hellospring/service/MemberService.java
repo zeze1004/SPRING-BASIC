a9e7c98c1563b16eefba5aba5f2645765b48a66c
zeze1004 <thwjd2717@naver.com>
2021-02-05 10:15:01 +0900

    add sign up code

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -4,13 +4,20 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 
+import java.util.Optional;
+
 public class MemberService {
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     // 회원가입
     public Long join(Member member) {
         // 같은 이름이 있는 중복 회원 x
-        memberRepository.findByName();
+        Optional<Member> result = memberRepository.findByName(member.getName());
+        result.ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+
+
         memberRepository.save(member);
         return member.getId();
     }
