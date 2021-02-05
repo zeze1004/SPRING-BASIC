2f2462c9e1de67d117cd8ac2568d802590b47ed4
zeze1004 <thwjd2717@naver.com>
2021-02-05 10:36:59 +0900

    add member inquiry func

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -4,6 +4,7 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
@@ -11,15 +12,28 @@ public class MemberService {
 
     // 회원가입
     public Long join(Member member) {
-        // 같은 이름이 있는 중복 회원 x
-        Optional<Member> result = memberRepository.findByName(member.getName());
-        result.ifPresent(m -> {
-            throw new IllegalStateException("이미 존재하는 회원입니다.");
-        });
+        // 증복 회원 검증
+        extracted(member);
 
 
         memberRepository.save(member);
         return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        Optional<Member> result = memberRepository.findByName(member.getName());
+        // 값이 있으면 throw
+        result.ifPresent(m -> { // meber를 m으로 받아서 값이 있는지 비교
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+    }
+    // 전체 회원 조회
+    public List<Member> findMembers() {
+        // emberRepository.findAll()의 return 값은 list
+        return memberRepository.findAll();
+    }
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
     }
 
 }
